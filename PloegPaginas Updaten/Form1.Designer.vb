685c85ff7698a291023794c7e5a6b65399748f06
Jan <Jan@BUREAU>
2017-07-12 22:26:25 +0200

    Add project files.

---
+++ PloegPaginas Updaten/Form1.Designer.vb
@@ -0,0 +1,146 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PloegenCmb = New System.Windows.Forms.ComboBox()
+        Me.MaakPaginaCodeBtn = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SchrijfNaarWordPressBtn = New System.Windows.Forms.Button()
+        Me.CopyBtn = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PaginaIDlbl = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'PloegenCmb
+        '
+        Me.PloegenCmb.FormattingEnabled = True
+        Me.PloegenCmb.Location = New System.Drawing.Point(37, 29)
+        Me.PloegenCmb.Name = "PloegenCmb"
+        Me.PloegenCmb.Size = New System.Drawing.Size(227, 21)
+        Me.PloegenCmb.TabIndex = 0
+        '
+        'MaakPaginaCodeBtn
+        '
+        Me.MaakPaginaCodeBtn.Location = New System.Drawing.Point(37, 56)
+        Me.MaakPaginaCodeBtn.Name = "MaakPaginaCodeBtn"
+        Me.MaakPaginaCodeBtn.Size = New System.Drawing.Size(169, 26)
+        Me.MaakPaginaCodeBtn.TabIndex = 1
+        Me.MaakPaginaCodeBtn.Text = "Maak Pagina Code"
+        Me.MaakPaginaCodeBtn.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(33, 141)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(1278, 621)
+        Me.TextBox1.TabIndex = 2
+        '
+        'SchrijfNaarWordPressBtn
+        '
+        Me.SchrijfNaarWordPressBtn.Location = New System.Drawing.Point(237, 56)
+        Me.SchrijfNaarWordPressBtn.Name = "SchrijfNaarWordPressBtn"
+        Me.SchrijfNaarWordPressBtn.Size = New System.Drawing.Size(155, 26)
+        Me.SchrijfNaarWordPressBtn.TabIndex = 3
+        Me.SchrijfNaarWordPressBtn.Text = "Schrijf Naar Wordpress"
+        Me.SchrijfNaarWordPressBtn.UseVisualStyleBackColor = True
+        '
+        'CopyBtn
+        '
+        Me.CopyBtn.Location = New System.Drawing.Point(432, 56)
+        Me.CopyBtn.Name = "CopyBtn"
+        Me.CopyBtn.Size = New System.Drawing.Size(143, 26)
+        Me.CopyBtn.TabIndex = 4
+        Me.CopyBtn.Text = "Copy"
+        Me.CopyBtn.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(40, 96)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(118, 24)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Test Pagina"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(164, 107)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(111, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Wordpress Pagina ID:"
+        '
+        'PaginaIDlbl
+        '
+        Me.PaginaIDlbl.AutoSize = True
+        Me.PaginaIDlbl.Location = New System.Drawing.Point(281, 107)
+        Me.PaginaIDlbl.Name = "PaginaIDlbl"
+        Me.PaginaIDlbl.Size = New System.Drawing.Size(13, 13)
+        Me.PaginaIDlbl.TabIndex = 7
+        Me.PaginaIDlbl.Text = "0"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(432, 101)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(142, 28)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "Batch Sync"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1356, 856)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.PaginaIDlbl)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.CopyBtn)
+        Me.Controls.Add(Me.SchrijfNaarWordPressBtn)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.MaakPaginaCodeBtn)
+        Me.Controls.Add(Me.PloegenCmb)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PloegenCmb As ComboBox
+    Friend WithEvents MaakPaginaCodeBtn As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents SchrijfNaarWordPressBtn As Button
+    Friend WithEvents CopyBtn As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PaginaIDlbl As Label
+    Friend WithEvents Button2 As Button
+End Class
---
+++ PloegPaginas Updaten/Form1.vb
@@ -0,0 +1,276 @@
+﻿Imports System.Data.OleDb
+Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim SQL As String = "SELECT Ploeg.PloegOrde, Ploeg.PloegNaam, Ploeg.ID FROM(Ploeg) WHERE ((Ploeg.actief)=Yes) ORDER BY Ploeg.PloegOrde "
+        Dim cn As New OleDbConnection("provider=microsoft.jet.oledb.4.0;data source=S:\basket\Wordpressprogramma\wordpress.mdb")
+        Dim dt As New DataTable()
+        Dim da As New OleDbDataAdapter()
+        cn.Open()
+        da.SelectCommand = New OleDbCommand((SQL), cn)
+        da.Fill(dt)
+        PloegenCmb.DataSource = dt
+        PloegenCmb.DisplayMember = "PloegNaam"
+        PloegenCmb.ValueMember = "ID"
+        cn.Close()
+    End Sub
+    Private Sub MaakPaginaCode()
+        TextBox1.Text = ""
+        Dim SqlCom As OleDbCommand
+        Dim cn As New OleDbConnection("provider=microsoft.jet.oledb.4.0;data source=S:\basket\Wordpressprogramma\wordpress.mdb")
+        Dim PloegID As Long = PloegenCmb.SelectedValue
+        Dim Dr As OleDbDataReader
+        Dim PloegNaam As String
+        Dim Reeks As String
+        Dim SponsorID As Long
+        Dim WPLayerSliderID As Long
+        Dim VBLGuid As String
+        Dim WPPloegPage As Long
+        Dim TrainingsUren As String
+        Dim FlickerSetID As String
+        Dim Foto As String
+        Dim SponsorEMail As String
+        Dim SponsorWebsite As String
+        Dim Sponsor As String
+        Dim SponsorKort As String
+
+        Dim Sb As New System.Text.StringBuilder
+
+
+        Try
+            cn.Open()
+            'Inlezen Ploeg Info
+            SqlCom = New OleDbCommand("SELECT Ploeg.ID, Ploeg.PloegNaam, Ploeg.Reeks, Ploeg.SponsorID, Ploeg.WPLayerSliderID, Ploeg.VBLGuid, Ploeg.WPPloegPage, Ploeg.Trainingsuren, Ploeg.FlickerSetID, Ploeg.Foto FROM Ploeg WHERE (((Ploeg.ID)=[@PLID]))", cn)
+            SqlCom.Parameters.AddWithValue("@PLID", PloegID)
+            Dr = SqlCom.ExecuteReader
+            If Not Dr.HasRows Then
+                Beep()
+                MsgBox("Geen ploeg gevonden!")
+                cn.Close()
+                Exit Sub
+            End If
+            Dr.Read()
+            PloegNaam = Dr("PloegNaam")
+            Reeks = Dr("Reeks")
+            SponsorID = Dr("SponsorID")
+            WPLayerSliderID = Dr("WPLayerSliderID")
+            VBLGuid = Dr("VBLGUID")
+            WPPloegPage = Dr("WPPloegPage")
+            PaginaIDlbl.Text = WPPloegPage.ToString
+            TrainingsUren = Dr("TrainingsUren")
+            FlickerSetID = Dr("FlickerSetID")
+            Foto = Dr("Foto")
+            'Inlezen sponsorinfo
+            SqlCom = New OleDbCommand("SELECT Gegevens.GegID, Gegevens.Titel, Gegevens.Adres, Gegevens.Postnummer, Gegevens.Gemeente, Gegevens.[Telefoon Zaak], Gegevens.Website, Gegevens.PubliekEmail
+FROM Ploeg LEFT JOIN Gegevens ON Ploeg.SponsorID = Gegevens.GEGID
+WHERE (((Ploeg.ID)=[@PLID]))", cn)
+            SqlCom.Parameters.AddWithValue("@PLID", PloegID)
+            Dr = SqlCom.ExecuteReader
+            If Not Dr.HasRows Then
+                Beep()
+                MsgBox("Geen sponsor gevonden!")
+                cn.Close()
+                Exit Sub
+            End If
+            Dr.Read()
+            Sb.AppendLine(Dr("Titel"))
+            Sb.AppendLine(Dr("Adres"))
+            Sb.Append(Dr("Postnummer"))
+            Sb.Append(" ")
+            Sb.AppendLine(Dr("Gemeente"))
+            Sb.Append("Tel:")
+            Sb.Append(Dr("Telefoon Zaak"))
+            Sponsor = Sb.ToString
+            Sb.Clear()
+            'SponsorEMail = Dr("PubliekEmail")
+            If IsDBNull(Dr("Website")) Then SponsorWebsite = "http://kbbco.be/" Else SponsorWebsite = Dr("Website")
+            SponsorID = Dr("GegID")
+            SponsorKort = Dr("Titel")
+            'Start Stringbuilding alle " staan dubbel=enkel in output
+            With Sb
+                .Append("[layerslider id=""")
+                .Append(WPLayerSliderID.ToString)
+                .AppendLine("""]")
+                .AppendLine()
+                .AppendLine("[tabby title=""Ploeg""]")
+                .AppendLine()
+                .AppendLine("[bscolumns class=""one_third""]")
+
+                ' Spelers inlezen
+                SqlCom = New OleDbCommand("SELECT Medewerkers.Naam, Medewerkers.Voornaam, Medewerkers.Geboorte
+FROM (Medewerkers RIGHT JOIN T_Groep ON Medewerkers.IDMW = T_Groep.IDMW) LEFT JOIN Ploeg ON T_Groep.IDPL = Ploeg.ID
+WHERE (((Ploeg.ID)=[@PLID]) AND ((Medewerkers.[Niet Aktief])=No) AND ((T_Groep.IDTI)=11))
+ORDER BY Medewerkers.Naam, Medewerkers.Voornaam", cn)
+                SqlCom.Parameters.AddWithValue("@PLID", PloegID)
+                Dr = SqlCom.ExecuteReader
+                If Dr.HasRows Then
+                    .AppendLine("<h4>Spelers</h4>")
+                    Do While Dr.Read
+                        .Append(Dr("Naam"))
+                        .Append(" ")
+                        .Append(Dr("Voornaam"))
+                        If IsDBNull(Dr("Geboorte")) Then
+                            .AppendLine()
+                        Else
+                            .Append("(")
+                            .Append(Year(Dr("Geboorte")).ToString)
+                            .AppendLine(")")
+                        End If
+                    Loop
+                End If
+                .AppendLine("[/bscolumns]")
+                .AppendLine()
+                .AppendLine("[bscolumns class=""one_third""]")
+                .AppendLine("<h4>Medewerkers</h4>")
+                'Andere medewerkers inlezen
+                SqlCom = New OleDbCommand("SELECT Medewerkers.Naam, Medewerkers.Voornaam, TitelOmschrijving.Titel
+FROM ((Medewerkers RIGHT JOIN T_Groep ON Medewerkers.IDMW = T_Groep.IDMW) LEFT JOIN Ploeg ON T_Groep.IDPL = Ploeg.ID) INNER JOIN TitelOmschrijving ON T_Groep.IDTI = TitelOmschrijving.IDTI
+WHERE (((Ploeg.ID)=[@PLID]) AND ((Medewerkers.[Niet Aktief])=No) AND ((T_Groep.IDTI)<>11))
+ORDER BY TitelOmschrijving.TitelOrde, Medewerkers.Naam, Medewerkers.Voornaam", cn)
+                SqlCom.Parameters.AddWithValue("@PLID", PloegID)
+                Dr = SqlCom.ExecuteReader
+                Dim VorigeTitel As String = ""
+                If Dr.HasRows Then
+                    Do While Dr.Read
+                        If Dr("Titel") <> VorigeTitel Then
+                            'Nieuw kop titel
+                            .Append("<h6>")
+                            .Append(Dr("Titel"))
+                            .AppendLine("</h6>")
+                            VorigeTitel = Dr("Titel")
+                        End If
+                        .Append(Dr("Naam"))
+                        .Append(" ")
+                        .AppendLine(Dr("Voornaam"))
+                    Loop
+                End If
+                .AppendLine("[/bscolumns]")
+                .AppendLine()
+                .AppendLine("[bscolumns class=""one_third_last""]")
+                .AppendLine("<h4>Ploegsponsor</h4>")
+                'Reclame inlezen om hoogte en breedt te bepalen
+                Dim tClient As New System.Net.WebClient
+                Dim ReclameUrl As String = "http://kbbco.be/wp-content/Sponsorlogos/" + SponsorID.ToString + ".jpg"
+                Dim tImage As Bitmap = Bitmap.FromStream(New System.IO.MemoryStream(tClient.DownloadData(ReclameUrl)))
+                Dim width As Integer = tImage.Width
+                Dim height As Integer = tImage.Height
+                tImage.Dispose()
+                .Append("<a href=""")
+                .Append(SponsorWebsite)
+                .Append("""")
+                .Append("><img class=""alignnone size-full wp-image-2489"" src=""/wp-content/Sponsorlogos/")
+                .Append(SponsorID.ToString)
+                .Append(".jpg"" alt=""")
+                .Append(SponsorKort)
+                .Append(""" width=""")
+                .Append(width.ToString)
+                .Append(""" height=""")
+                .Append(height.ToString)
+                .AppendLine(""" /></a>")
+                .AppendLine()
+                .AppendLine(Sponsor)
+                .Append("<a href=""")
+                .Append(SponsorWebsite)
+                .AppendLine(""">Bezoek Website Sponsor </a>")
+                '.Append("<a href=""mailto:")
+                '.Append(SponsorEMail)
+                '.Append("?subject=Vraag via KBBCO.be")
+                '.Append(""">")
+                ' .AppendLine("Mail Sponsor </a>")
+                .AppendLine()
+                .AppendLine("[/bscolumns]")
+                .AppendLine()
+                If VBLGuid <> "nihil" Then
+                    .AppendLine("[tabby title=""Kalender""]")
+                    .Append("[kalender guid=""")
+                    .Append(VBLGuid)
+                    .Append(""" naam=""")
+                    .Append(PloegNaam)
+                    .AppendLine("""]")
+                    .AppendLine()
+                    .AppendLine("[tabby title=""Klassement""]")
+                    .Append("[klassement guid=""")
+                    .Append(VBLGuid)
+                    .Append(""" naam=""")
+                    .Append(PloegNaam)
+                    .AppendLine("""]")
+                    .AppendLine()
+                End If
+                .AppendLine("[tabby title=""Verslagen""]")
+                .AppendLine("[catlist categorypage=""yes"" tags=""verslag""]")
+                .AppendLine()
+                .AppendLine("[tabby title=""Ploegfoto""]")
+                .Append("<img src=""http://kbbco.be/wp-content/PloegFotos/")
+                .Append(Foto)
+                .AppendLine(".jpg"" alt=""Ploegfoto"" />")
+                .AppendLine()
+                .AppendLine("[tabby title=""Trainingsuren""]")
+                .AppendLine()
+                .AppendLine(TrainingsUren)
+                .AppendLine("[tabby title=""Foto's""]")
+                .Append("<div style=""position: relative; padding-bottom: 101%; height: 0; overflow: hidden;""><iframe id=""iframe"" style=""width: 100%; height: 100%; position: absolute; top: 0; left: 0;"" src=""http://flickrit.com/slideshowholder.php?height=100&amp;size=big&amp;count=100&amp;setId=")
+                .Append(FlickerSetID.ToString)
+                .AppendLine("&amp;click=true&amp;thumbnails=1&amp;transition=0&amp;layoutType=responsive&amp;sort=0"" width=""300"" height=""150"" frameborder=""0"" scrolling=""no""></iframe></div>")
+                .AppendLine("[tabbyending]")
+
+            End With
+
+
+            TextBox1.Text = Sb.ToString
+            cn.Close()
+
+
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+            If cn.State = ConnectionState.Open Then cn.Close()
+        End Try
+
+
+    End Sub
+
+    Private Sub MaakPaginaCodeBtn_Click(sender As Object, e As EventArgs) Handles MaakPaginaCodeBtn.Click
+        MaakPaginaCode()
+    End Sub
+
+    Private Sub CopyBtn_Click(sender As Object, e As EventArgs) Handles CopyBtn.Click
+        Clipboard.SetText(TextBox1.Text)
+    End Sub
+    Private Sub SchrijfNaarWordPress()
+        Dim MySqlConn As New MySqlConnection
+        MySqlConn.ConnectionString = "server=mysql038.hosting.combell.com; user=ID147483_wpress; password=p6dVNY6r; database=ID147483_wpress"
+        MySqlConn.Open()
+        Dim mscomm As New MySqlCommand("UPDATE wp_posts SET wp_posts.post_content = @post_content
+WHERE (((wp_posts.ID)=@PostID));", MySqlConn)
+
+        mscomm.Parameters.AddWithValue("@PostID", PaginaIDlbl.Text)
+        mscomm.Parameters.AddWithValue("@post_content", TextBox1.Text)
+        Dim i As Integer = mscomm.ExecuteNonQuery()
+        If i = 1 Then Beep() Else MsgBox("Fout:" + i.ToString + " Rijen aangepast in database")
+
+
+        MySqlConn.Close()
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Process.Start("http://kbbco.be/?page_id=" + PaginaIDlbl.Text)
+    End Sub
+
+    Private Sub PloegenCmb_SelectedIndexChanged(sender As Object, e As EventArgs) Handles PloegenCmb.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        For i = 0 To PloegenCmb.Items.Count - 1
+            PloegenCmb.SelectedIndex = i
+            MaakPaginaCode()
+            SchrijfNaarWordPress()
+        Next
+    End Sub
+
+    Private Sub SchrijfNaarWordPressBtn_Click(sender As Object, e As EventArgs) Handles SchrijfNaarWordPressBtn.Click
+        SchrijfNaarWordPress()
+    End Sub
+End Class
